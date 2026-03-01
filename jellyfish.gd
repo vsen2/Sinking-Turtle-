0ac8c89098160f5557085dcda9e43d872b5a172b
vsen2 <victorlucassen@gmail.com>
2026-03-01 09:43:23 -0500

    more stuff turtle

--- jellyfish.gd
+++ jellyfish.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
-@export var speed: float = 300.0  # Pixels per second
+@export var speed: float = 100
 
 func _process(delta: float):
-	# 1. Move the node downward
-	position.y += speed * delta
+	# 1. Move the node UP
+	position.y -= speed * delta
 	
-	# 2. Get the height of the viewport
-	var screen_height = get_viewport_rect().size.y
+	# 2. Get the screen dimensions
+	var viewport_size = get_viewport_rect().size
 	
-	# 3. Check if it has moved past the bottom
-	if position.y > screen_height:
-		position.y = 0  # Reset to the top
+	# 3. Check if it hit the top (y < 0)
+	if position.y < 0:
+		# Reset to the bottom
+		position.y = viewport_size.y
+		
+		# Move to a random X position between 0 and the screen width
+		position.x = randf_range(0, viewport_size.x)
